82befcd1ff659d32b279e3907b013678cccdb100
spialelo <ndphillips@gmail.com>
2017-01-03 22:55:59 -0500

    Copying over work from project repo to 100 days of code repo. Will continue working on original repo and use this as a backup, during the challenge.

---
+++ js/myConcept.js
@@ -0,0 +1,209 @@
+/**
+ * Created by Nicole on 10/6/2015.
+ */
+
+
+(function(){
+
+
+    /* makeupBag array of contents */
+    var makeupBag = [];
+
+
+    /* General makeup object */
+    function Makeup(brand, mkname, mktype, expDate){
+        this.brand = brand;
+        this.mkname = mkname;
+        this.mktype = mktype;
+        this.expDate = expDate;
+    }
+    /* end of General makeup object */
+
+    function toInitCap(str)
+    {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+
+
+
+    /* Adding things to the bag */
+    function addToBag(){
+
+        var inputBrand = toInitCap(document.getElementById("mkBrand").value);
+        var inputName = toInitCap(document.getElementById("mkName").value);
+        var inputType = toInitCap(document.getElementById("mkType").value);
+        var inputExpDate = document.getElementById("mkExpDate").value;
+
+
+        /*conver string entered for the date, into a date object and find expiration dates of objects by calculation*/
+
+
+        if(inputBrand != "" && inputName != "" && inputType != "" && inputExpDate != "") {
+
+            var product_1 = new Makeup(inputBrand,inputName,inputType,inputExpDate);
+            makeupBag.push(product_1);
+            document.getElementById("feedbackMakeup").textContent = makeupBag.length + " item(s) in your makeup bag.";
+
+        }
+
+        else if(inputBrand == "" || inputName == "" || inputType == "" || inputExpDate == ""){
+
+            document.getElementById("displayBag").textContent = "Add something to your make up bag. :)";
+
+        }
+
+        document.getElementById("mkBrand").value = "";
+        document.getElementById("mkName").value = "";
+        document.getElementById("mkType").value = "";
+        document.getElementById("mkExpDate").value = "";
+
+    }
+    /* end of Adding things to the bag */
+
+
+    /* display what is in the bag */
+    function showBag (){
+      for(var i = 0; i < makeupBag.length; i++){
+          document.getElementById("displayBag").innerHTML += makeupBag[i].brand + " " +makeupBag[i].mkname +"<br>";
+      }
+    }
+    /* end of display what is in the bag */
+
+
+    /* search the bag/array of objects */
+    function searchAll (matchSearch){
+        this.searchMatch = matchSearch;
+        this.searchByBrand = function(){
+            var searchResults = " ";
+            for(var j = 0; j < makeupBag.length; j++){
+                    if (this.searchMatch == makeupBag[j].brand){
+                        searchResults += "You have "+makeupBag[j].brand + " "+makeupBag[j].mkname+ " in your makeup bag. It has an expiration date of "+makeupBag[j].expDate+". It is a "+makeupBag[j].mktype+".<br>";
+                    }
+                }
+
+            document.getElementById("displayBag").innerHTML = searchResults;
+            console.log("searching by brand for "+ this.searchMatch);
+            console.log(searchResults);  
+        };
+        this.searchByName = function(){
+            var searchResults = " ";
+            for(var j = 0; j < makeupBag.length; j++){
+                    if (this.searchMatch == makeupBag[j].mkname){
+                        searchResults += "You have "+makeupBag[j].brand + " "+makeupBag[j].mkname+ " in your makeup bag. It has an expiration date of "+makeupBag[j].expDate+". It is a "+makeupBag[j].mktype+".<br>";
+                    }
+                }
+
+            document.getElementById("displayBag").innerHTML = searchResults;
+            console.log("searching by name for "+ this.searchMatch);
+            console.log(searchResults);
+        };
+        this.searchByType = function(){
+            var searchResults = " ";
+            for(var j = 0; j < makeupBag.length; j++){
+                    if (this.searchMatch == makeupBag[j].mktype){
+                        searchResults += "You have "+makeupBag[j].brand + " "+makeupBag[j].mkname+ " in your makeup bag. It has an expiration date of "+makeupBag[j].expDate+". It is a "+makeupBag[j].mktype+".<br>";
+                    }
+                }
+
+            document.getElementById("displayBag").innerHTML = searchResults;
+            console.log("searching by type for "+ this.searchMatch);
+            console.log(searchResults);
+        };
+        this.searchByExpDate = function(){
+            var searchResults = " ";
+            for(var j = 0; j < makeupBag.length; j++){
+                    if (this.searchMatch == makeupBag[j].expDate){
+                        searchResults += "You have "+makeupBag[j].brand + " "+makeupBag[j].mkname+ " in your makeup bag. It has an expiration date of "+makeupBag[j].expDate+". It is a "+makeupBag[j].mktype+".<br>";
+                    }
+                }
+
+            document.getElementById("displayBag").innerHTML = searchResults;
+            console.log("searching by expiration date for "+ this.searchMatch);
+            console.log(searchResults);
+        };
+    }
+    /* search the bag/array of objects - further plans to make the search more robust, case insensitive, split string at the space to pull in more results when searching by name, etc*/
+
+    function searchMkBag(){        
+        var sel = document.getElementById("searchDrop");
+        var dropDownSelected = sel.options[sel.selectedIndex].value;
+        var searchFor = document.getElementById("searchInput").value;
+        var brandSearch = new searchAll(searchFor);
+
+        switch(dropDownSelected){
+            case "makeBrand":
+                return brandSearch.searchByBrand();
+                break;
+            case "makeName":
+                return brandSearch.searchByName();
+                break;
+            case "makeType":
+                return brandSearch.searchByType();
+                break;
+            case "makeExpDate":
+                return brandSearch.searchByExpDate();
+                break;
+            default:
+                console.log("Select one of the values from the drop down.");
+                break;
+
+
+        }
+
+    }
+
+
+
+    var buttonAddMakeup = document.getElementById("addMakeupButton");
+    buttonAddMakeup.onclick = addToBag;
+
+    var displayMakeupBag = document.getElementById("showMakeupButton");
+    displayMakeupBag.onclick = showBag;
+
+    var searchTheBag = document.getElementById("searchByButton");
+    searchTheBag.onclick = searchMkBag;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*starting all over */
+
+    function eraseDiv() {
+
+        var displayField = document.getElementById("displayBag");
+        var feedbackField = document.getElementById("feedbackMakeup");
+
+        /*Reset the display area to an empty square.*/
+        displayField.textContent = "";
+        feedbackField.textContent = "";
+
+        var form = document.getElementById("makeupForm");
+
+        /*Reset form values.*/
+        form.reset();
+
+        /*Reset the display area size to the original values.*/
+        displayField.style.width = "400px";
+        displayField.style.height = "400px";
+
+    }
+
+    var resetButton = document.getElementById("resetButton");
+    resetButton.onclick = eraseDiv;
+
+})();
--- log.md
+++ log.md
@@ -3,28 +3,28 @@
 ### Day 0: February 30, 2016 (Example 1)
 ##### (delete me or comment me out)
 
-**Today's Progress**: Fixed CSS, worked on canvas functionality for the app.
+#**Today's Progress**: Fixed CSS, worked on canvas functionality for the app.
 
-**Thoughts:** I really struggled with CSS, but, overall, I feel like I am slowly getting better at it. Canvas is still new for me, but I managed to figure out some basic functionality.
+#**Thoughts:** I really struggled with CSS, but, overall, I feel like I am slowly getting better at it. Canvas is still new for me, but I managed to figure out some basic functionality.
 
-**Link to work:** [Calculator App](http://www.example.com)
-
-### Day 0: February 30, 2016 (Example 2)
-##### (delete me or comment me out)
-
-**Today's Progress**: Fixed CSS, worked on canvas functionality for the app.
-
-**Thoughts**: I really struggled with CSS, but, overall, I feel like I am slowly getting better at it. Canvas is still new for me, but I managed to figure out some basic functionality.
-
-**Link(s) to work**: [Calculator App](http://www.example.com)
+#**Link to work:** [Calculator App](http://www.example.com)
 
 
 ### Day 1: June 27, Monday
 
-**Today's Progress**: I've gone through many exercises on FreeCodeCamp.
+#**Today's Progress**: I've gone through many exercises on FreeCodeCamp.
 
-**Thoughts** I've recently started coding, and it's a great feeling when I finally solve an algorithm challenge after a lot of attempts and hours spent.
+#**Thoughts** I've recently started coding, and it's a great feeling when I finally solve an algorithm challenge after a lot of attempts and hours spent.
 
-**Link(s) to work**
-1. [Find the Longest Word in a String](https://www.freecodecamp.com/challenges/find-the-longest-word-in-a-string)
-2. [Title Case a Sentence](https://www.freecodecamp.com/challenges/title-case-a-sentence)
+#**Link(s) to work**
+#1. [Find the Longest Word in a String](https://www.freecodecamp.com/challenges/find-the-longest-word-in-a-string)
+#2. [Title Case a Sentence](https://www.freecodecamp.com/challenges/title-case-a-sentence)
+
+
+#---------------- 100 Days of Code - Log - Nicole Phillips --------------#
+
+### Day 0: Work up to January 2,2017
+**Concept: My personal makeup/cosmetics bag or vanity to carry around to keep track of what's in my stash**
+### Day 1: January 3, 2017
+**Today's Progress**: Built out the remaining methods for my searchAll object.
+**Thoughts** Method searchByName. There is room for improvement. My idea: split the input based on whitespace and see if any of the substrings of the input have a pattern that matches any party of the string value stored in the makeup name property at the index in the loop. 
