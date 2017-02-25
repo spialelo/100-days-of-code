6ac8e700792bd913d4cc203ff889f626935d4541
spialelo <ndphillips@gmail.com>
2017-02-24 22:51:52 -0500

    Also started work on ES6 course

---
+++ es6/js/var.js
@@ -0,0 +1,17 @@
+function setWidth() {
+    var width = 100;
+    console.log(width);
+}
+
+setWidth();
+
+
+var age = 100;
+
+if (age > 12) {
+    const dogYears = age * 7;
+    console.log(`You are ${dogYears} dog years old!`);
+}
+
+let height = 200;
+const key = 'abc123';
